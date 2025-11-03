25b7f741b3c55f3dcd052cabd00d3b356cf9f7be
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2025-11-02 16:19:02 -0800

    vercel.json added

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
   base:process.env.VITE_BASE_PATH ||"/",//remove this line when you are working on localhost
+   plugins: [react(), tailwindcss(),],
 })
